e49285d0c8e7ebf01ef8177f8c93fbc16fcecfed
Marawan-Ahmed <memoahmedrabee@yahoo.com>
2022-11-28 23:33:41 +0200

    fixed some errors

--- assignment4/polynomials.py
+++ assignment4/polynomials.py
@@ -1,4 +1,127 @@
-from SingleLinkedList import *
+class Node:
+    def __init__(self, data=None):
+        self.data = data
+        self.next = None
+
+
+class SingleLinkedList:
+    def __init__(self):
+        self.head = None
+        self.size = 0
+
+    def add_last(self, data):
+        new_node = Node(data)
+        if self.head is None:
+            self.head = new_node
+        else:
+            p = self.head
+            while p.next:
+                p = p.next
+            p.next = new_node
+        self.size += 1
+
+    def add_index(self, data, index):
+        if index > self.size or index < 0:
+            raise TypeError("")
+        new_node = Node(data)
+        if index == 0:
+            new_node.next = self.head
+            self.head = new_node
+        else:
+            p = self.head
+            for i in range(0, index - 1):
+                p = p.next
+            u = p.next
+            new_node.next = u
+            p.next = new_node
+        self.size += 1
+
+    def list_print(self):
+        p = self.head
+        print("[", end=""),
+        while p:
+            if p.next:
+                print(p.data, end=", ")
+            else:
+                print(p.data, end="")
+            p = p.next
+        print("]")
+
+    def get_element(self, index):
+        if self.not_in_bound(index):
+            raise TypeError("")
+        p = self.head
+        for i in range(0, index):
+            p = p.next
+        return p.data
+
+    def set_element(self, data, index):
+        if self.not_in_bound(index):
+            raise TypeError("")
+        p = self.head
+        for i in range(0, index):
+            p = p.next
+        p.data = data
+
+    def not_in_bound(self, index) -> bool:
+        return index >= self.size or index < 0
+
+    def delete_element(self, index):
+        if self.not_in_bound(index):
+            raise TypeError("")
+
+        if index == 0:
+            p = self.head
+            self.head = p.next
+        else:
+            cur = self.head
+            prev = None
+            for i in range(0, index):
+                prev = cur
+                cur = cur.next
+            prev.next = cur.next
+        self.size -= 1
+
+    def clear(self):
+        # while (self.size > 0):
+        #   self.deleteElement(0)
+        self.head = None
+        self.size = 0
+
+    def sublist(self, start_index, end_index):
+        if self.not_in_bound(start_index) or \
+                self.not_in_bound(end_index) or \
+                end_index < start_index:
+            raise TypeError("")
+        p = self.head
+        for i in range(0, start_index):
+            p = p.next
+        new_sublist = SingleLinkedList()
+        for i in range(start_index, end_index + 1):
+            new_sublist.add_last(p.data)
+            p = p.next
+        return new_sublist
+
+    def is_empty(self):
+        if self.size == 0 or self.head is None:
+            return True
+        return False
+
+    def contains(self, data):
+        p = self.head
+        for i in range(self.size):
+            if p.data == data:
+                return True
+            p = p.next
+        return False
+
+    def initialize(self, data_):
+        if data_ != "[]":
+            data_ = data_[1:-1]
+            data_ = data_.replace(" ", "").split(',')
+            for i in range(len(data_)):
+                self.add_last(data_[i])
+
 
 
 
@@ -93,13 +216,18 @@ def print_equation(F1):
         coeff = int(F1.get_element(size - power - 1))
         if coeff == 0:
             continue
-        temp.append(coeff_format(coeff))
-        temp.append(power_format(power))
+        if (coeff == 1) and (power == size - 1):
+            temp.append(power_format(power))
+        else:    
+            temp.append(coeff_format(coeff))
+            temp.append(power_format(power))
     temp[0] = temp[0].lstrip("+")
     return ''.join(temp)
 
 
 def coeff_format(coeff):
+    if coeff == 1:
+        return "+"
     return str(coeff) if coeff < 0 else "+{0}".format(coeff)
 
 
@@ -112,105 +240,116 @@ def power_format(power):
         return 'x^{0}'.format(power)
 
 def return_var(input):
-    if input == "A" or "a":
+    if (input == "A") or (input == "a"):
         return A
-    if input == "B" or "b":
+    elif (input == "B") or (input == "b"):
         return B
-    if input == "C" or "c":
+    elif (input == "C") or (input == "c"):
         return C
-    if input == "R" or "r":
+    elif (input == "R") or (input == "r"):
         return R
+    else:
+        raise Exception
+
+# if __name__ == "__main__":
+
+#     global R
+#     R = SingleLinkedList()
+#     print_times = 0
+#     try:
+#         while (1): 
+#             command = input() 
+#             if command == "set":
+#                 sym = input()
+#                 if sym not in ["A", "a", "B", "b", "C", "c", "R", "r"]:
+#                     raise TypeError("")
+#                 if (sym == "A") or (sym == "a"):
+#                     Aa = input()
+#                     global A
+#                     A = SingleLinkedList()
+#                     A.initialize(Aa)
+#                 elif (sym == "B") or (sym == "b"):
+#                     Bb = input()
+#                     global B
+#                     B = SingleLinkedList()
+#                     B.initialize(Bb)
+#                 elif (sym == "C") or (sym == "c"):
+#                     Cc = input()
+#                     global C
+#                     C = SingleLinkedList()
+#                     C.initialize(Cc)
+#                 else:
+#                     raise TypeError("")
+
+#             elif command == "print":
+#                 letter_print = input()
+#                 if letter_print not in ["A", "a", "B", "b", "C", "c", "R", "r"]:
+#                     raise TypeError("")
+#                 if (letter_print == "A") or (letter_print == "a"):
+#                     print(print_equation(A))
+#                 elif (letter_print == "B") or (letter_print == "b"):
+#                     print(print_equation(B))
+#                 elif (letter_print == "C") or (letter_print == "c"):
+#                     print(print_equation(C))
+#                 elif (letter_print == "R") or (letter_print == "r"):
+#                     print(print_equation(R))
+#                 print_times+=1
+#                 if print_times == 3:
+#                     break
+
+#             elif command == "eval":
+#                 eval_letter = input()
+#                 value = input()
+#                 if (eval_letter == "A") or (eval_letter == "a"):
+#                     print(eval(A, value))
+#                 elif (eval_letter == "B") or (eval_letter == "b"):
+#                     print(eval(B, value))
+#                 elif (eval_letter == "C") or (eval_letter == "c"):
+#                     print(eval(C, value))
+#                 break
+
+#             elif command == "add":
+#                 operand1 = return_var(input())
+#                 operand2 = return_var(input())
+#                 R = add(operand1, operand2)
+#                 print(print_equation(R))
+#                 break
+
+#             elif command == "sub":
+#                 operand1 = return_var(input())
+#                 operand2 = return_var(input())
+#                 R = sub(operand1, operand2)
+#                 print(print_equation(R))
+#                 break
+
+#             elif command == "mult":
+#                 operand1 = return_var(input())
+#                 operand2 = return_var(input())
+#                 R = mul(operand1, operand2)
+#                 print(print_equation(R))
+#                 break
+
+#             elif command == "clear":
+#                 clear_letter = return_var(input())
+#                 print([])
+#                 break
+#             else:
+#                 raise Exception 
+#     except:
+#         print("Error")
 
 
-if __name__ == "__main__":
-
-    command = input()
-    global R
-    R = SingleLinkedList()
-    try:
-        if command == "set":
-            sym = input()
-            if sym not in ["A", "a", "B", "b", "C", "c", "R", "r"]:
-                raise TypeError("")
-            if sym == "A" or "a":
-                Aa = input()
-                global A
-                A = SingleLinkedList()
-                A.initialize(Aa)
-            elif sym == "B" or "b":
-                Bb = input()
-                global B
-                B = SingleLinkedList()
-                B.initialize(Bb)
-            elif sym == "C" or "c":
-                Cc = input()
-                global C
-                C = SingleLinkedList()
-                C.initialize(Cc)
-            else:
-                raise TypeError("")
-
-        if command == "print":
-            letter_print = input()
-            if letter_print not in ["A", "a", "B", "b", "C", "c", "R", "r"]:
-                raise TypeError("")
-            if letter_print == "A" or "a":
-                print(print_equation(A))
-            if letter_print == "B" or "b":
-                print(print_equation(B))
-            if letter_print == "C" or "c":
-                print(print_equation(C))
-            if letter_print == "R" or "r":
-                print(print_equation(R))
-
-        if command == "eval":
-            eval_letter = input()
-            value = input()
-            if eval_letter == "A" or "a":
-                print(eval(A, value))
-            if eval_letter == "B" or "b":
-                print(eval(B, value))
-            if eval_letter == "C" or "c":
-                print(eval(C, value))
-
-        if command == "add":
-            operand1 = return_var(input())
-            operand2 = return_var(input())
-            R = add(operand1, operand2)
-            print(print_equation(R))
-
-        if command == "sub":
-            operand1 = return_var(input())
-            operand2 = return_var(input())
-            R = add(operand1, operand2)
-            print(print_equation(R))
-
-        if command == "mult":
-            operand1 = return_var(input())
-            operand2 = return_var(input())
-            R = add(operand1, operand2)
-            print(print_equation(R))
-
-        if command == "clear":
-            clear_letter = return_var(input())
-            print(clear(clear_letter))
-        else:
-            print("Error")
-    except TypeError:
-        print("Error")
 
 
 
+p1 = SingleLinkedList()
+p1.initialize("[32, 41, 67]")
 
+p2 = SingleLinkedList()
+p2.initialize("[2, 3, 1]")
 
-#p1 = SingleLinkedList()
-#p1.initialize("[32, 41, 67]")
-
-#p2 = SingleLinkedList()
-#p2.initialize("[2, 3, 1]")
-
-#sum = mul(p1, p2)
-#sum.list_print()
-#print(eval(p2, 2))
-#p1.list_print()
-#print(print_equation(p1))
+sum = mul(p1, p2)
+sum.list_print()
+print(eval(p2, 2))
+p1.list_print()
+print(print_equation(p1))
